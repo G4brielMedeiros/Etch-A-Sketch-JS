bae2dfef8432963fedbb3852e7140c74eb4ac26e
Gabriel Medeiros <g4brielmedeiros@pm.me>
2021-11-18 18:59:57 -0300

    fixed css, added sketching feature

--- docs/script.js
+++ docs/script.js
@@ -1,18 +1,23 @@
 const sketchboardDIV = document.querySelector(".sketchboard");
+const boardSize = 16 ;
 
 function createDiv() {
   const sketchCellDIV = document.createElement("div");
-//sketchCellDIV.innerText = "foo";
   sketchCellDIV.classList = "cell";
+
+  sketchCellDIV.addEventListener("mouseover", () => {
+    sketchCellDIV.classList = "cell cell-black";
+  });
+
   return sketchCellDIV;
 }
 
 function createColumn() {
   let sketchColumnDIV = document.createElement("div");
   sketchColumnDIV.classList = "column";
-  for (let i = 0; i < 16; i++) sketchColumnDIV.appendChild(createDiv());
+  for (let i = 0; i < boardSize; i++) sketchColumnDIV.appendChild(createDiv());
 
   sketchboardDIV.appendChild(sketchColumnDIV);
 }
 
-for (let i = 0; i < 16; i++) createColumn();
+for (let i = 0; i < boardSize; i++) createColumn();
